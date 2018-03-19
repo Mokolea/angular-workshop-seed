ff4957aef16f6e0e14d28e9dffb4de3abf22036d
Mario Ban <mario.ban@bluewin.ch>
2018-03-19 16:14:34 +0100

    01 1-8

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import {BrowserModule} from '@angular/platform-browser';
 
 import {AppComponent} from './app.component';
 import {HomeComponent} from './home/home.component';
+import {FlightSearchComponent} from './flight-search/flight-search.component';
 
 @NgModule({
   declarations: [
@@ -11,7 +12,8 @@ import {HomeComponent} from './home/home.component';
     // Directives
     // Pipes
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    FlightSearchComponent
   ],
   exports: [
     // Modules
---
+++ src/app/flight-search/flight-search.component.ts
@@ -0,0 +1,9 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'flight-search',
+  templateUrl: './flight-search.component.html'
+})
+export class FlightSearchComponent {
+
+}
